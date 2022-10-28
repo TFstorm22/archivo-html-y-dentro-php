0a84b63b9865f0b87ffbb8f0000056e4bf342c67
Jose Torres Flores <115846091+TFstorm22@users.noreply.github.com>
2022-10-28 18:58:40 -0500

    Update ejercicio3.php

--- ejercicio3.php
+++ ejercicio3.php
@@ -1,8 +1,18 @@
-<?php //Programa de tabla de multiplicar 1 al 6
+
+<?php //Programa de tabla de multiplicar 1 al 6 1ra forma
 for ($tabla=1; $tabla <=6 ; $tabla++){
 for ($x=1; $x <=6;$x++){
 	echo '[ '.$tabla.' * '.$x.' = '.$tabla*$x.' ] ';
 }
 echo nl2br("\r\n");
 } 
+?>
+
+<?php //Programa de tabla de multiplicar 1 al 6 2da forma
+for($x=1;$x<7;$x++){
+    for($y=1;$y<7;$y++){    
+echo strval($x*$y) . " ";
+    }
+    echo nl2br("\r\n");
+}
 ?>
