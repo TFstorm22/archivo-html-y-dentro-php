c40d937aad6966158b1935daf2bd99d2c62f82c1
Jose Torres Flores <115846091+TFstorm22@users.noreply.github.com>
2022-10-28 12:45:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
     echo '<html>'
-    <head>
-    <title>PHP Test</title>
-    </head>
-    <body>
-    
-<p> Hello World</P>
-
-    </body>
-
-
-
+     <head>
+        <title>PHP Test</title>
+     </head>
+        <body>
+         <p> Hello World</P>
+        </body>
     </html>
     ?>
